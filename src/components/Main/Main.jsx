f320724b57c6033f99c35644ad6a2a962ebb2183
Joshua Gomez <gomezjoshuam@gmail.com>
2025-06-22 21:06:26 -0400

    apply some styles

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,16 @@
 import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
+import "./Main.css";
 
 function Main({ items, weather, onCardClick }) {
   return (
     <main className="main">
       <WeatherCard weather={weather} />
-      {items.map((item) => (
-        <ItemCard item={item} key={item._id} onCardClick={onCardClick} />
-      ))}
+      <div className="main__item-cards">
+        {items.map((item) => (
+          <ItemCard item={item} key={item._id} onCardClick={onCardClick} />
+        ))}
+      </div>
     </main>
   );
 }
