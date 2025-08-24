b7984fa548078c4ffd5e13e69c395b730c07bde7
Joshua Gomez <gomezjoshuam@gmail.com>
2025-08-24 14:26:45 -0400

    project 15 changes

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,8 +1,8 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import "./ItemModal.css";
-import Modal from "../Modal/Modal";
 import ConfirmDeleteModal from "../ConfirmDeleteModal/ConfirmDeleteModal";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
+import closeIcon from "../../assets/close.svg";
 
 function ItemModal({ item, onClose, onDeleteCard, isLoading }) {
   const [isConfirmDeleteOpen, setIsConfirmDeleteOpen] = useState(false);
@@ -10,35 +10,66 @@ function ItemModal({ item, onClose, onDeleteCard, isLoading }) {
 
   const isOwn = item?.owner === currentUser?._id;
 
+  useEffect(() => {
+    if (!item) return;
+
+    const handleEscape = (e) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("keydown", handleEscape);
+    return () => document.removeEventListener("keydown", handleEscape);
+  }, [item, onClose]);
+
+  const handleOverlay = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  if (!item) return null;
+
   return (
     <>
-      <Modal name="item" onClose={onClose} isOpen={!!item}>
-        <img
-          src={item?.imageUrl}
-          alt={item?.name}
-          className="item-modal__image"
-        />
-        <div className="item-modal__content">
-          <div className="item-modal__caption">
-            <p className="item-modal__caption-name">{item?.name}</p>
-            <p className="item-modal__caption-weather">
-              Weather: {item?.weather}
-            </p>
-          </div>
-          <div className="item-modal__button">
-            {isOwn && (
-              <button
-                className="item-modal__button-delete"
-                onClick={() => {
-                  setIsConfirmDeleteOpen(true);
-                }}
-              >
-                Delete item
-              </button>
-            )}
+      <div
+        className="item-modal"
+        onClick={handleOverlay}
+        role="dialog"
+        aria-modal="true"
+      >
+        <div className="item-modal__container">
+          <button className="item-modal__close" type="button" onClick={onClose}>
+            <img src={closeIcon} alt="close" />
+          </button>
+          <img
+            src={item?.imageUrl}
+            alt={item?.name}
+            className="item-modal__image"
+          />
+          <div className="item-modal__content">
+            <div className="item-modal__caption">
+              <p className="item-modal__caption-name">{item?.name}</p>
+              <p className="item-modal__caption-weather">
+                Weather: {item?.weather}
+              </p>
+            </div>
+            <div className="item-modal__button">
+              {isOwn && (
+                <button
+                  className="item-modal__button-delete"
+                  onClick={() => {
+                    setIsConfirmDeleteOpen(true);
+                  }}
+                >
+                  Delete item
+                </button>
+              )}
+            </div>
           </div>
         </div>
-      </Modal>
+      </div>
       {isConfirmDeleteOpen && (
         <ConfirmDeleteModal
           item={item}
